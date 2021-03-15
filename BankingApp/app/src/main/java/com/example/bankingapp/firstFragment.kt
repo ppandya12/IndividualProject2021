ba0a0aca2ea758fa516226fd09e637b800ae5cbe
priyanka pandya <zfac212@live.rhul.ac.uk>
2021-03-15 17:17:46 +0000

    first page created

--- BankingApp/app/src/main/java/com/example/bankingapp/firstFragment.kt
+++ BankingApp/app/src/main/java/com/example/bankingapp/firstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,16 @@ class firstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        // Link image button to transactions page
+        val root = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val imageButton2 = root.findViewById<View>(R.id.imageButton2) as ImageButton
+
+        imageButton2.setOnClickListener {
+            view?.let { it1 -> Navigation.findNavController(it1).navigate(R.id.action_firstFragment_to_secondFragment) }
+        }
+
+        return root
     }
 
     companion object {
